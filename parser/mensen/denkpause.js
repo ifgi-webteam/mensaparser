53391230449f683c463fbb78d0c32ea3e83090aa
Christoph Kisfeld <spam@b-web.org>
2015-02-10 14:55:22 +0100

    Denkpause Menü I und II

---
+++ parser/mensen/denkpause.js
@@ -0,0 +1,12 @@
+/*
+	Parser für die Mensa am Aasee
+*/
+
+var mensa = {
+			"name": "Bistro Denkpause",
+			"uid": "http://data.uni-muenster.de/context/uniaz/70ad738c960cc5e88e5e8d8acaaaaaaa",
+			"url": "http://www.studentenwerk-muenster.de/de/essen-a-trinken/bistros-a-cafes/denkpause"
+};
+
+var bistroParser = require("./parser_bistro_denkpause.js");
+var r = new bistroParser.bistroparser(mensa);
--- parser/mensen/parser_bistro_denkpause.js
+++ parser/mensen/parser_bistro_denkpause.js
@@ -60,45 +60,57 @@ function process(error, response, html) {
 		// Preise abfragen
 		// Ergebnisse in Variable preise: 
 		// preise[0] komplettes pattern match
-		// preise[1] (Menü 1 Student), 
-		// preise[2] (Menü 1 Sonst.), 
-		// preise[3] (Menü 2 Student),
-		// preise[4] (Menü 2 Sonst.),
-		// preise[5] (Menü 3 Student),
-		// preise[6] (Menü 3 Sonst.)
+		// preise[1] (Menü 1/2 Student), 
+		// preise[2] (Menü 1/2 Sonst.), 
+		// preise[3] (Menü 3 Student),
+		// preise[4] (Menü 3 Sonst.),
+		// preise[5] (Einopf Student),
+		// preise[6] (Eintopf Sonst.)
 		var content = $("table.contentpaneopen").text();
-		var preise = content.match(/[0P]reise\s+Stud. \/ Sonst. Menü I\s+([0-9],[0-9]+) € \/ ([0-9],[0-9]+) €Menü II\s+([0-9],[0-9]+) € \/ ([0-9],[0-9]+) €Menü III\s+([0-9],[0-9]+) € \/ ([0-9],[0-9]+) €/);
+		var preise = content.match(/Preise\sMenü\sI\s\/II\sStud\.\s([0-9],[0-9]+)\s€\/Sonst\.\s([0-9],[0-9]+)\s€Preise\sMenü\sIII\s+Stud\.\s([0-9],[0-9]+)\s€\/Sonst\.\s([0-9],[0-9]+)\s€\s+Eintopf\s+Stud\.\s([0-9],[0-9]+)\s€\/Sonst\.\s([0-9],[0-9]+)\s€/);
 
 		// iterate idList as week days
 		for (weekDay in idList) { // mo, di, mi, do, fr
 			// Datum des Tages parsen
 			var dateToday = $( '#' + idList[weekDay] ).text().split(" ").pop().replace("\r\n", "");
 			
-			// iterate over the 3 daily menus
-			for(var i = 1; i <= 3; i++) {
-				// JSON Objekt für jedes Menü
-				var fooditem = {
-					"mensa": {
-						"name": mensa.name,
-						"uid": mensa.uid
-					},
-					"date": moment(dateToday, "DD.MM.YYYY").format('YYYY-MM-DD'),
-					"name": $( '#' + idList[weekDay] + "_menu" + i ).text(),
-					"minPrice": preise[i*2-1],
-					"maxPrice": preise[i*2]
-				};
-				console.log("" + fooditem.date + ": " + fooditem.name);
-				insertData(fooditem);
-			}
+			// JSON Objekt für jedes Menü
+			// Menü I
+			var fooditem = {
+				"mensa": {
+					"name": mensa.name,
+					"uid": mensa.uid
+				},
+				"date": moment(dateToday, "DD.MM.YYYY").format('YYYY-MM-DD'),
+				"name": $( '#' + idList[weekDay] + "_menu1" ).text(),
+				"minPrice": preise[1],
+				"maxPrice": preise[2]
+			};
+			console.log("" + fooditem.date + ": " + fooditem.name + " (" + fooditem.minPrice + "/" + fooditem.maxPrice + ")");
+			
+			// Menü II
+			var fooditem = {
+				"mensa": {
+					"name": mensa.name,
+					"uid": mensa.uid
+				},
+				"date": moment(dateToday, "DD.MM.YYYY").format('YYYY-MM-DD'),
+				"name": $( '#' + idList[weekDay] + "_menu2" ).text(),
+				"minPrice": preise[1],
+				"maxPrice": preise[2]
+			};
+			console.log("" + fooditem.date + ": " + fooditem.name + " (" + fooditem.minPrice + "/" + fooditem.maxPrice + ")");
+
+			// insertData(fooditem);
 		}
 	}
 }
 
 // main method for module export & start point
-function ringparser(mens) {
+function bistroparser(mens) {
 	var request = require('request');
 	mensa = mens;
 	request(mensa.url, process);
 }
 
-module.exports.ringparser = ringparser;
+module.exports.bistroparser = bistroparser;
