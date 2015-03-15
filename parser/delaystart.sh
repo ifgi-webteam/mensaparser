c7eada35e408c81c0e50d8ac54c60def8c713389
Christoph Kisfeld <spam@b-web.org>
2015-03-15 13:59:07 +0100

    Add weekly cronjob Scripts borrowed from https://github.com/webwurst/docker-go-cron

--- parser/delaystart.sh
+++ parser/delaystart.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 echo Waiting 12 seconds...
+
+# create cron job to run every sunday
+# "@weekly" according to https://godoc.org/github.com/robfig/cron
+go-cron '@weekly' /bin/sh -c 'node server.js' &
+
+# run once on startup
+# use "sleep infinity" to keep Docker container alive
 sleep 12 && node server.js && sleep infinity
--- parser/mensen/parser_aasee_ring.js
+++ parser/mensen/parser_aasee_ring.js
@@ -71,7 +71,7 @@ var ringparser = function(mensa) {
 			// preise[5] (Menü 3 Student),
 			// preise[6] (Menü 3 Sonst.)
 			var content = $("table.contentpaneopen").text();
-			console.log(content);
+			//console.log(content);
 			var preise = content.match(/[0P]reise\s*Stud.\s*\/\s*Sonst.\s*Menü\s*I\s*([0-9],[0-9]+)\s*€\s*\/\s*([0-9],[0-9]+)\s*€\s*Menü\s*II\s*([0-9],[0-9]+)\s*€\s*\/\s*([0-9],[0-9]+)\s*€\s*Menü\s*III\s*([0-9],[0-9]+)\s*€\s*\/\s*([0-9],[0-9]+)\s*€/);
 
 			// iterate idList as week days
