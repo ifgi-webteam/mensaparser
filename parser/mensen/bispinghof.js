adc1d2ccc541451ab965d7cd83b6d27e81063726
Christoph Kisfeld <spam@b-web.org>
2015-05-31 11:53:21 +0200

    Mensa Bispinghof

---
+++ parser/mensen/bispinghof.js
@@ -0,0 +1,12 @@
+/*
+	Parser für die Mensa Bispinghof
+*/
+
+var mensa = {
+			"name": "Mensa Bispinghof",
+			"uid": "http://data.uni-muenster.de/context/uniaz/MensaBispinghof",
+			"url": "http://www.studentenwerk-muenster.de/essen-a-trinken/mensen/bispinghof"
+};
+
+var bispinghofParser = require("./parser_bispinghof.js");
+var r = new bispinghofParser.bispinghofparser(mensa);
